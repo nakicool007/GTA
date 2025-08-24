2e366097823357c84e7107bb20813390ed590f23
Nakul Kumbria <142852407+nakicool007@users.noreply.github.com>
2025-08-23 18:27:52 -0700

    Delete Global Tennis Academy directory

--- Global Tennis Academy/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('contactForm')?.addEventListener('submit', function (e) {
-    e.preventDefault();
-    document.getElementById('formMessage').textContent = 'Thank you for your message! We will get back to you soon.';
-    this.reset();
-  });
-  
